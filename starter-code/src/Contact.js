aee1953cfd421b297e8909c63d9c91947e6fc563
DSauthier <danielsauthier@live.com>
2018-12-04 17:37:08 -0500

    react logic done, not styled yet

---
+++ starter-code/src/Contact.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import contacts from './contactList.json'
+
+class Contact extends Component {
+  state = {
+    contacts: contacts,
+    someContacts: contacts.slice(0, 5)
+    // popularity: contacts.popularity
+  };
+
+  addRandomCelebrity = () => {
+    const randomCeleb = [...this.state.contacts]; // copies main array of celebs
+    const someOfTheContacts = [...this.state.someContacts]; // copies the slice of arrays
+    const randomNum = Math.floor(Math.random() * this.state.contacts.length); // randomizes the index to grab
+    const newThing = randomCeleb[randomNum]; // grabs the index of a random Celeb
+    const extraNewThing = randomCeleb[randomNum + 1];
+
+    for (let i = 0; i < someOfTheContacts.length; i++) {
+      if (newThing === someOfTheContacts[i]) {
+        someOfTheContacts.push(extraNewThing);
+      }
+    }
+    someOfTheContacts.push(newThing); // adds the random celeb to the previous array
+
+    this.setState({
+      //changes the state to add the new random person
+      someContacts: someOfTheContacts
+    });
+  };
+
+  sortingCelebsbyPopularity = () => {
+    // console.log("hello")
+    const listOfContacts = [...this.state.someContacts];
+    listOfContacts.sort((firstCeleb, secondCeleb) => {
+      if (firstCeleb.popularity > secondCeleb.popularity) {
+        return -1;
+      } else if (firstCeleb.popularity < secondCeleb.popularity) {
+        return 1;
+      }
+      return 0;
+    });
+
+    this.setState({
+      //changes the state to add the new random person
+      someContacts: listOfContacts
+    });
+  };
+  sortingCelebsbyName = () => {
+    // console.log("hello")
+    const listOfContacts = [...this.state.someContacts];
+    listOfContacts.sort((firstCeleb, secondCeleb) => {
+      if (firstCeleb.name > secondCeleb.name) {
+        return 1;
+      } else if (firstCeleb.name < secondCeleb.name) {
+        return -1;
+      }
+      return 0;
+    });
+
+    this.setState({
+      //changes the state to add the new random person
+      someContacts: listOfContacts
+    });
+  };
+  deleteCeleb = (i) => {
+
+    const newContactList = this.state.someContacts;
+    newContactList.splice(i, 1); //Remove ith hero from the array
+    this.setState({ //Replace the state 
+      someContacts: newContactList
+    })
+  }
+
+  render() {
+    const contactList = this.state.someContacts.map((ourCeleb, index) => {
+      return <li key={ourCeleb.index}>
+          <h2>Name: {ourCeleb.name}</h2>
+          <img src={ourCeleb.pictureUrl} />
+          <h4>Popularity: {ourCeleb.popularity}</h4>
+          <button onClick={() => this.deleteCeleb(index)}>
+            Delete Celebrity
+          </button>
+        </li>;
+    });
+
+    return (
+      <div className="eachCeleb">
+        <button onClick={() => this.addRandomCelebrity()}>
+          Add Random Contact
+        </button>
+        <button onClick={() => this.sortingCelebsbyPopularity()}>
+          sort by Popularity
+        </button>
+        <button onClick={() => this.sortingCelebsbyName()}>sort by Name</button>
+
+        {contactList}
+      </div>
+    );
+  }
+}
+
+export default Contact;
+
+
+
